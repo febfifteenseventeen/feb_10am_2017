fe98fe3d1ae4d0b46a310c78c92ca6d911ee06cf
venkatgn <venkatadrin.g@gmail.com>
2017-02-15 11:37:26 +0530

    updated home program with new f2 method and few lines in f1 method

--- My10amproj/src/main/java/yahoo/Home.java
+++ My10amproj/src/main/java/yahoo/Home.java
@@ -5,5 +5,10 @@ public class Home
   public void f1()
   {
 	  System.out.println("Test ");
+	  System.out.println("hello");
+  }
+  public void f2()
+  {
+	  System.out.println("for tesitng");
   }
 }
